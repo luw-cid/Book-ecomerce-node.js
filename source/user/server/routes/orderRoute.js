d0f88495001568c3e7ccd57f763a015fdf96a65a
MinhQuan0206 <nguyentranminhquan02062005@gmail.com>
2025-11-23 01:22:24 +0700

    implement: view orders history in profile of user

--- source/user/server/routes/orderRoute.js
+++ source/user/server/routes/orderRoute.js
@@ -2,11 +2,15 @@ const express = require('express');
 const router = express.Router();
 const asyncHandle = require('express-async-handler');
 const orderController = require('../controllers/orderControler');
+const { authMiddleware } = require('../middlewares/authMiddleware');
 
+// POST / - Create order (cho phép guest checkout, không bắt buộc auth)
 router.post('/', asyncHandle(orderController.createOrder));
-router.get('/my-orders', asyncHandle(orderController.getUserOrders));
-router.get('/stats', asyncHandle(orderController.getUserOrderStats));
-router.get('/:id', asyncHandle(orderController.getOrderById));
-router.patch('/:id/cancel', asyncHandle(orderController.cancelOrder));
+
+// Các routes sau đây cần authentication
+router.get('/my-orders', authMiddleware, asyncHandle(orderController.getUserOrders));
+router.get('/stats', authMiddleware, asyncHandle(orderController.getUserOrderStats));
+router.get('/:id', authMiddleware, asyncHandle(orderController.getOrderById));
+router.patch('/:id/cancel', authMiddleware, asyncHandle(orderController.cancelOrder));
 
 module.exports = router;
